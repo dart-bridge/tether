b3dbfb011081bdbb129eb926bac10cd05ab051a3
Emil Persson <emil.n.persson@gmail.com>
2015-11-20 13:37:00 +0100

    Comment and enable all master factories to receive external session

--- lib/src/anchors/simple_anchor.dart
+++ lib/src/anchors/simple_anchor.dart
@@ -3,14 +3,23 @@ part of tether.protocol;
 class SimpleAnchor implements Anchor {
   final Sink<String> _sink;
   final Stream<String> _stream;
+  final StreamController<String> _controller = new StreamController();
 
-  SimpleAnchor(this._sink, this._stream);
+  SimpleAnchor(this._sink, this._stream) {
+    _listen();
+  }
+
+  Future _listen() async {
+    await for (final payload in _stream)
+        _controller.add(payload);
+    await _controller.close();
+  }
 
   Sink<String> get sink => _sink;
 
-  Stream<String> get stream => _stream;
+  Stream<String> get stream => _controller.stream;
 
-  bool get isOpen => true;
+  bool get isOpen => _controller.isClosed;
 
   Future get onClose async => new Completer().future;
 
--- lib/src/tether.dart
+++ lib/src/tether.dart
@@ -1,6 +1,15 @@
 part of tether;
 
 abstract class Tether {
+  /// Creates a new master [Tether] from an [Anchor].
+  ///
+  /// The [session] parameter receives a [Session] object for external
+  /// session handling. If no [session] is provided, a new [Session]
+  /// will be generated.
+  ///
+  /// The [reconnect] parameter can be assigned a [Reconnect] strategy
+  /// for re-establishing the connection. This behaviour is not yet
+  /// implemented. TODO: implement Reconnect
   factory Tether.masterAnchor(Anchor anchor,
       {Session session,
       Reconnect reconnect}) {
@@ -15,6 +24,9 @@ abstract class Tether {
     return tether;
   }
 
+  /// Creates a new slave [Tether] from an [Anchor].
+  ///
+  /// The slave will receive its [Session] from the master.
   factory Tether.slaveAnchor(Anchor anchor, {Reconnect reconnect}) {
     final messenger = new Messenger(anchor);
     final tether = new _Tether(reconnect: reconnect);
@@ -24,47 +36,81 @@ abstract class Tether {
     return tether;
   }
 
-  factory Tether.spawnIsolate(void body(ports)) {
-    return new Tether.masterAnchor(new IsolateAnchor.spawn(body));
+  /// Creates a new master [Tether] and spawn a new isolate, passing
+  /// it the ports required to attach to the slave [Tether].
+  factory Tether.spawnIsolate(void body(ports), {Session session}) {
+    return new Tether.masterAnchor(
+        new IsolateAnchor.spawn(body), session: session);
   }
 
-  factory Tether.spawnIsolateUri(Uri uri, [List<String> arguments = const []]) {
-    return new Tether.masterAnchor(new IsolateAnchor.spawnUri(uri, arguments));
+  /// Creates a new master [Tether] and spawn an external isolate,
+  /// passing it the ports required to attach the the slave.
+  ///
+  /// Receives [arguments] for the spawned file to receive.
+  factory Tether.spawnIsolateUri(Uri uri,
+      {List<String> arguments: const [], Session session}) {
+    return new Tether.masterAnchor(
+        new IsolateAnchor.spawnUri(uri, arguments), session: session);
   }
 
+  /// Creates a new slave [Tether] by connecting to a master isolate,
+  /// using the ports received by [Tether.spawnIsolate] or [Tether.spawnIsolateUri].
   factory Tether.connectIsolate(ports) {
     return new Tether.slaveAnchor(new IsolateAnchor.connect(ports));
   }
 
-  factory Tether.master(Sink<String> sink, Stream<String> stream) {
+  /// Creates a simple master [Tether] by simply providing a [sink] and a [stream].
+  factory Tether.master(Sink<String> sink, Stream<String> stream,
+      {Session session}) {
     final anchor = new SimpleAnchor(sink, stream);
-    return new Tether.masterAnchor(anchor);
+    return new Tether.masterAnchor(anchor, session: session);
   }
 
+  /// Creates a simple slave [Tether] by simply providing a [sink] and a [stream].
   factory Tether.slave(Sink<String> sink, Stream<String> stream) {
     final anchor = new SimpleAnchor(sink, stream);
     return new Tether.slaveAnchor(anchor);
   }
 
+  /// Sends a [payload] to the paired [Tether] with a [key]. The other side must [listen]
+  /// to the same key to respond. The return value from that handler will be returned
+  /// in the returned [Future] from this method.
   Future send(String key, [payload]);
 
+  /// Semantic alias for using [send] with no payload.
+  Future get(String key);
+
+  /// Listens to a [key] on the open connection. Each time the paired [Tether] sends a
+  /// payload to the same key, this [listener] will be used to react on, and respond to,
+  /// that request. The return value from the [listener] will be sent back to the other
+  /// side, if it's not null.
   StreamSubscription listen(String key, Function listener);
 
+  /// The same as listening to a key and immediately cancel the subscription on
+  /// the first payload.
   void listenOnce(String key, Function listener);
 
+  /// A broadcasting stream that sends a ping each time the connection is (re-)established.
   Stream get onConnectionEstablished;
 
+  /// A broadcasting stream that sends a ping each time the connection is lost.
   Stream get onConnectionLost;
 
+  /// A future that completes the next time the [Tether] is connected.
   Future get onConnection;
 
+  /// Checks if the [Tether] is connected.
   bool get isConnected;
 
+  /// Get the unique [Session] that this is authorized with.
   Session get session;
 
+  /// Close the [Tether] connection, as well as the underlying [Anchor].
   void close();
 }
 
+/// Enables a [Tether] to support reconnection, by providing a way
+/// to get a new [Messenger] using the same [Session].
 typedef Future<Messenger> Reconnect(Session session);
 
 class _Tether implements Tether {
@@ -80,6 +126,8 @@ class _Tether implements Tether {
   Future send(String key, [payload]) {
     return _messenger.send(key, payload);
   }
+
+  Future get(String key) => send(key);
 
   StreamSubscription listen(String key, Function listener) {
     return _messenger.listen(key, listener);
