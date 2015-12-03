21d6aff205f7832097a6f7f07653cf4b727f3b07
Emil Persson <emil.n.persson@gmail.com>
2015-12-03 23:04:10 +0100

    Hook with framework codebase

--- lib/src/session.dart
+++ lib/src/session.dart
@@ -1,14 +1,25 @@
 part of tether.protocol;
 
-class Session {
-  final String id;
-  final Map data;
+typedef Session SessionFactory(String id, Map data);
 
-  const Session(this.id, this.data);
+abstract class Session {
+  final String id;
+  Map data;
+
+  static SessionFactory factory = (String id, Map data) => new _Session(id, data);
+
+  factory Session(String id, Map data) => factory(id, data);
 
   factory Session.generate(Map data) {
-    return new Session(generateHash(length: 128), new Map.from(data));
+    return factory(generateHash(length: 128), new Map.from(data));
   }
+}
+
+class _Session implements Session {
+  final String id;
+  Map data;
+
+  _Session(this.id, this.data);
 
   operator ==(Session other) => other is Session && other.id == id;
 }
